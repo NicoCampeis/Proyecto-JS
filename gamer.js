250afeb5eaa39aec55c2bdbceb74ee5ede70e02d
Nicolas Campeis <nico.campeis@hotmail.com.ar>
2022-05-01 10:56:04 -0300

    proyecto gamer

---
+++ gamer.js
@@ -0,0 +1,25 @@
+//te creas una lista de objetos, de juegos, con su nombre, precio por unidad, cantidad de stock
+
+ class Juegos {
+    constructor(nombre,precio, genero, stock) {
+        this.nombre = nombre;
+        this.genero = genero;
+        this.precio = parseFloat(precio);
+        this.stock = stock;
+        this.vendido = false;
+    }
+    precioJuego(){
+        console.log (this.nombre + ([this.precio * 1.21] ) + " pesos");
+    };
+
+}
+
+const game = [];
+game.push(new Juegos("f1 ", " 300"));
+game.push(new Juegos("gta-v " , " 1000"));
+game.push(new Juegos("gran turismo ", " 500"));
+game.push(new Juegos("Dragon ball Z ", " 700"));
+
+/*  for(const juego of game ) {
+    juego.precioJuego();
+}; */
